99d885be60a9748898c57088642fa22c602e0ab7
nareshit20 <65172771+nareshit20@users.noreply.github.com>
2021-06-30 07:40:23 +0530

    modified

--- interpolation/variables.tf
+++ interpolation/variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
-    type = "string"
+    type = string
     default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-    type = "string"
+    type = string
     default = "10.1.0.0/16"
 }
 
 variable "vpc_name" {
-    type = "string"
+    type = string
     default = "qa.example.com"
 }
 
